9ea3ee9b8cee377097b054e30f19b3ad5cacbbb1
seonae <sunellie55@gmail.com>
2021-05-27 01:35:52 +0900

    인사말, 고객센터 css 수정

--- src/main/java/sogon/booksys/controller/HomeController.java
+++ src/main/java/sogon/booksys/controller/HomeController.java
@@ -50,136 +50,48 @@ public class HomeController {
 
     @GetMapping("/membership/card")
     public String card(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user!=null) {
-            model.addAttribute("userName", user.getName());
-
-            model.addAttribute("userEmail", user.getEmail());
-
-            Role role = userRepository.findByEmail(user.getEmail()).get().getRole();
-            log.info("Role = {}", role);
-            if(role == Role.ADMIN)
-                model.addAttribute("userRole", role);
-        }
         return "membership/card";
     }
     @GetMapping("/membership/event")
     public String event(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user!=null) {
-            model.addAttribute("userName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-
-            Role role = userRepository.findByEmail(user.getEmail()).get().getRole();
-            log.info("Role = {}", role);
-            if(role == Role.ADMIN)
-                model.addAttribute("userRole", role);
-        }
         return "membership/event";
     }
     @GetMapping("/membership/coupon")
     public String coupon(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user!=null) {
-            model.addAttribute("userName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-
-            Role role = userRepository.findByEmail(user.getEmail()).get().getRole();
-            log.info("Role = {}", role);
-            if(role == Role.ADMIN)
-                model.addAttribute("userRole", role);
-        }
         return "membership/coupon";
     }
     @GetMapping("/customercenter/center")
     public String center(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user!=null) {
-            model.addAttribute("userName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-            Role role = userRepository.findByEmail(user.getEmail()).get().getRole();
-            log.info("Role = {}", role);
-            if(role == Role.ADMIN)
-                model.addAttribute("userRole", role);
-        }
         return "customercenter/center";
     }
     @GetMapping("/customercenter/onetoone")
     public String onetoone(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user!=null) {
-            model.addAttribute("userName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-            Role role = userRepository.findByEmail(user.getEmail()).get().getRole();
-            log.info("Role = {}", role);
-            if(role == Role.ADMIN)
-                model.addAttribute("userRole", role);
-        }
         return "customercenter/onetoone";
     }
     @GetMapping("/restaurant/Hi")
     public String Hi(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user!=null) {
-            model.addAttribute("userName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-            Role role = userRepository.findByEmail(user.getEmail()).get().getRole();
-            log.info("Role = {}", role);
-            if(role == Role.ADMIN)
-                model.addAttribute("userRole", role);
-        }
         return "restaurant/Hi";
+    }
+    @GetMapping("/reservation/statistics")
+    public String statistics(Model model){
+        return "reservation/statistics";
     }
     @GetMapping("/restaurant/introduce")
     public String introduce(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user!=null) {
-            model.addAttribute("userName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-            Role role = userRepository.findByEmail(user.getEmail()).get().getRole();
-            log.info("Role = {}", role);
-            if(role == Role.ADMIN)
-                model.addAttribute("userRole", role);
-        }
         return "restaurant/introduce";
     }
     @GetMapping("/restaurant/location")
     public String location(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user!=null) {
-            model.addAttribute("userName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-            Role role = userRepository.findByEmail(user.getEmail()).get().getRole();
-            log.info("Role = {}", role);
-            if(role == Role.ADMIN)
-                model.addAttribute("userRole", role);
-        }
         return "restaurant/location";
     }
     @GetMapping("/menu/lunch")
     public String lunch(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user!=null) {
-            model.addAttribute("userName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-            Role role = userRepository.findByEmail(user.getEmail()).get().getRole();
-            log.info("Role = {}", role);
-            if(role == Role.ADMIN)
-                model.addAttribute("userRole", role);
-        }
+
         return "menu/lunch";
     }
     @GetMapping("/menu/dinner")
     public String dinner(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user!=null) {
-            model.addAttribute("userName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-            Role role = userRepository.findByEmail(user.getEmail()).get().getRole();
-            log.info("Role = {}", role);
-            if(role == Role.ADMIN)
-                model.addAttribute("userRole", role);
-        }
+
         return "menu/dinner";
     }
 }
