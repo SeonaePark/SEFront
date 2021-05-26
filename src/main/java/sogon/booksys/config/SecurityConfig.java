f4d5135d8136865a35ee9b9d58603c6853d363aa
seonae <sunellie55@gmail.com>
2021-05-27 01:43:59 +0900

    home css 수정

--- src/main/java/sogon/booksys/config/SecurityConfig.java
+++ src/main/java/sogon/booksys/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/","/login","/restaurant/Hi","/restaurant/introduce",
                         "/restaurant/location","/reservation/statistics","/menu/lunch","/menu/dinner","/membership/card",
-                        "/customercenter/center","/membership/event", "/css/**","/js/**","/image/**").permitAll()
+                        "/customercenter/center","/membership/event", "/membership/coupon","/css/**","/js/**","/image/**").permitAll()
                 .antMatchers("/tables/**","/users").hasRole(Role.ADMIN.name())
                 .anyRequest().authenticated()
                 .and()
